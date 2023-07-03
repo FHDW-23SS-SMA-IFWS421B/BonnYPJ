edd7b867e6af601424f563916e9b23d21e31da53
ToxiqJan <jan.hoeltje@gmx.net>
2023-07-03 13:37:16 +0200

    added weather api

---
+++ src/Bots/WeatherBot.java
@@ -0,0 +1,6 @@
+package Bots;
+
+public class WeatherBot {
+    private static final String API_KEY = "e6409848f08fd04167779a4c19729199";
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import ApiConnection.*;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.print("Enter a City name: ");
+
+        String cityName = myObj.nextLine();  // Read user input
+
+        try {
+            OpenWeatherMapAPI weatherAPI = new OpenWeatherMapAPI("e6409848f08fd04167779a4c19729199");
+            String response = weatherAPI.getCurrentWeather(cityName);
+            System.out.println(response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
