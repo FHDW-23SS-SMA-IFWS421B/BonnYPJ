35ab209d2bdf67582b5cc6122d125e2ac242bb78
ToxiqJan <jan.hoeltje@gmx.net>
2023-08-26 10:27:12 +0200

    WikiBot v1

---
+++ src/main/java/org/example/bots/WikiBot.java
@@ -0,0 +1,69 @@
+package org.example.bots;
+
+import org.example.bots.BotTemplate;
+import org.json.JSONObject;
+
+import org.example.exceptions.InvalidInputException;
+import org.example.apiConnection.APIConnect;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+public class WikiBot implements BotTemplate {
+
+    private static final String BASE_URL = "https://de.wikipedia.org/w/rest.php/v1/search/page?q=";
+
+    private APIConnect apiConnection = new APIConnect();
+    private Map<String, String> commands = new HashMap<>();
+    private String result = null;
+
+    public WikiBot(String input) throws InvalidInputException, UnsupportedEncodingException {
+        setupCommands();
+        String result = connection(input);
+        System.out.println(result);
+    }
+
+    @Override
+    public void setupCommands() {
+        commands.put("!wiki", "Mit dem zu suchenden Begriff, um Infos zu erhalten");
+    }
+
+    @Override
+    public String commandList() {
+        StringBuilder output = new StringBuilder("Willkommen beim Wiki-Bot. Dir stehen folgende Befehle zur Verfügung:\n");
+        for (String command : commands.keySet()) {
+            output.append(" - ").append(command).append(": ").append(commands.get(command)).append("\n");
+        }
+        return output.toString();
+    }
+
+    @Override
+    public String connection(String input) throws InvalidInputException {
+        JSONObject wikiData;
+
+        if (input.equals("!wiki")){
+            return commandList();
+        }
+
+        String[] command = input.split(" ");
+        String apiCommand = "";
+        for (int i = 1; i < command.length; i++){
+            apiCommand += command[i];
+        }
+        try {
+            String apiUrl = buildApiUrl(input);
+            wikiData = apiConnection.connectToApi(apiUrl, null);
+        }catch (UnsupportedEncodingException e) {
+            throw new InvalidInputException("Fehler beim Aufbau der API-URL.");
+        }
+
+        return wikiData.toString();
+    }
+
+    private String buildApiUrl(String input) throws UnsupportedEncodingException {
+        String encodedInput = URLEncoder.encode(input, "UTF-8");
+        return String.format("%s%s&limit=3", BASE_URL, encodedInput);
+    }
+}
