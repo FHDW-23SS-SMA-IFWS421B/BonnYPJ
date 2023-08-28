360929ebd80fe77616ad5491a4b7a5e98ea246a9
ToxiqJan <jan.hoeltje@gmx.net>
2023-08-28 11:39:17 +0200

    TranslateBot v2

--- src/main/java/org/example/bots/TranslateBot.java
+++ src/main/java/org/example/bots/TranslateBot.java
@@ -11,13 +11,14 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class TranslateBot implements BotTemplate{
+public class TranslateBot implements BotTemplate {
 
-    private static final String API_KEY = "";
-    private static final String CURRENT_WEATHER_API_URL = "";
+    private static final String API_KEY = "3dba9f7c-b663-2d6d-9ded-f5cb55afc8c2:fx";
+    private static final String BASE_URL = "https://api-free.deepl.com/v2/translate";
 
     private APIConnect apiConnection = new APIConnect();
     private Map<String, String> commands = new HashMap<>();
+    Map<String, String> languageMap = new HashMap<>();
     private String result = null;
 
 
@@ -29,12 +30,12 @@ public class TranslateBot implements BotTemplate{
 
     @Override
     public void setupCommands() {
-        commands.put("!translator", "+ [EN], [DE], etc. für die zu übersetzende Sprache.");
+        commands.put("!translator", "+ [DE], [EN], [ES], [FR], [IT] für die zu übersetzende Sprache.");
     }
 
     @Override
     public String commandList() {
-        StringBuilder output = new StringBuilder("Willkommen beim Übersetzerbot. Dir stehen folgende Befehle zur Verfügung:\n");
+        StringBuilder output = new StringBuilder("Willkommen beim Übersetzerbot. Dir steht folgender Befehl zur Verfügung\n");
         for (String command : commands.keySet()) {
             output.append(" - ").append(command).append(": ").append(commands.get(command)).append("\n");
         }
@@ -45,22 +46,34 @@ public class TranslateBot implements BotTemplate{
     public String connection(String input) throws InvalidInputException, UnsupportedEncodingException {
         JSONObject translateData;
 
+        if (input.equals("!translator")) {
+            return commandList();
+        }
+
         String[] command = input.split(" ");
 
-        Map<String, String> languageMap = new HashMap<>();
+
         languageMap.put("DE", "Deutsch");
         languageMap.put("EN", "Englisch");
-        languageMap.put("IT", "Italienisch");
         languageMap.put("ES", "Spanisch");
+        languageMap.put("FR", "Französisch");
+        languageMap.put("IT", "Italienisch");
 
-        //Scanner scanner = new Scanner(System.in);
-        //String userInput = scanner.nextLine().toUpperCase(); // Convert input to uppercase
 
-        String selectedLanguage = languageMap.get(command[1]);
+        String selectedLanguage = languageMap.get(command[1].toUpperCase());
 
         if (selectedLanguage != null) {
             String[] language = {selectedLanguage};
-            return "Selected language: " + language[0];
+            System.out.println("Selected language: " + language[0] + "\n Was möchtest du übersetzen lassen?");
+
+            Scanner scanner = new Scanner(System.in);
+            String text = scanner.nextLine();
+
+            String apiUrl = buildApiUrl(command[1], text);
+
+            JSONObject translatorData = apiConnection.connectToApi(apiUrl, null);
+            String output = jsonFormat(translatorData.toString());
+            return output;
         } else {
             return "Invalid language code.";
         }
@@ -70,6 +83,13 @@ public class TranslateBot implements BotTemplate{
     public String jsonFormat(String data) throws InvalidInputException {
         return null;
     }
+
+    private String buildApiUrl(String language, String text) throws UnsupportedEncodingException {
+        String encodedLanguage = URLEncoder.encode(language, "UTF-8");
+        String encodedText = URLEncoder.encode(text, "UTF-8");
+        return String.format("%s?auth_key=%s&text=%s&target_lang=%s", BASE_URL, API_KEY, encodedText, encodedLanguage);
+    }
+
 
     private String getLanguage() {
         return null;
